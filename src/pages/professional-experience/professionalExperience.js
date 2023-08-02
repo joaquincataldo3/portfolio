945d85159891f1146665c84a1957e0f24841f344
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-08-02 18:06:59 -0300

    adding new project

--- src/pages/professional-experience/professionalExperience.js
+++ src/pages/professional-experience/professionalExperience.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import mySqlLogo from '../../assets/images/mysqllogo.png'
 import quilmacProjectImg from '../../assets/images/quilmacprojectimg.jpg'
-import alterEgoProjectImg from '../../assets/images/alteregoimg.jpg'
+import iSmileImg from '../../assets/images/ismilelogo.png'
 import { useGlobalContext } from '../../hooks/context'
 import { Link } from 'react-router-dom'
 import '../projects/Projects.css'
@@ -20,6 +20,28 @@ function Projects() {
       <h1 className='projects-intro-title'>Professional experience</h1>
       <div className='projects-wrapper'>
 
+        <div className='project-container'>
+          <div className='project-img'>
+            <img src={iSmileImg} alt="Alter ego e-commerce" className='adjust-project-img' />
+          </div>
+          <div className='project-info'>
+            <h3 className='project-title'>I Smile e-commerce</h3>
+            <p className='project-description'>Currently co-developing/designing this freelance project for cloth brand. HTML -EJS template based-, and pure CSS/Javascript for the front
+              and MySQL with Sequelize and Javascript with AWS integration to create a CRUD of products and users with its respective validations/relations. In addition, we used several tools and libraries to develop different functionalities according to client requirements.
+            </p>
+
+            <div className='skills-used'>
+              <i class='bx bxl-javascript' ></i>
+              <i class='bx bxl-css3' ></i>
+              <img className='mysql-logo' src={mySqlLogo} alt="" />
+              <iconify-icon icon="file-icons:sequelize" style={{ color: "#E9E8E8", fontSize: '1.5rem' }}></iconify-icon>
+            </div>
+            <div className='project-links-container'>
+              <button className='project-link-btn'><Link to='https://ismile.com.ar' target='_blank'>DEPLOY</Link></button>
+              <button className='project-link-btn'><Link target='_blank' to='https://github.com/joaquincataldo3/proyecto-cherly'>GITHUB</Link></button>
+            </div>
+          </div>
+        </div>
 
         <div className='project-container'>
           <div className='project-img'>
@@ -44,28 +66,6 @@ function Projects() {
           </div>
         </div>
 
-        <div className='project-container'>
-          <div className='project-img'>
-            <img src={alterEgoProjectImg} alt="Alter ego e-commerce" className='adjust-project-img' />
-          </div>
-          <div className='project-info'>
-            <h3 className='project-title'>Alter ego e-commerce</h3>
-            <p className='project-description'>Currently co-developing/designing this freelance project for cloth brand. HTML -EJS template based-, and pure CSS/Javascript for the front
-              and MySQL with Sequelize and Javascript to create a CRUD of products and users with its respective validations/relations. Still working on an features/pages.
-            </p>
-
-            <div className='skills-used'>
-              <i class='bx bxl-javascript' ></i>
-              <i class='bx bxl-css3' ></i>
-              <img className='mysql-logo' src={mySqlLogo} alt="" />
-              <iconify-icon icon="file-icons:sequelize" style={{ color: "#E9E8E8", fontSize: '1.5rem' }}></iconify-icon>
-            </div>
-            <div className='project-links-container'>
-              <button className='project-link-btn'><Link to='#'>DEPLOY -NOT AVAILABLE-</Link></button>
-              <button className='project-link-btn'><Link target='_blank' to='https://github.com/joaquincataldo3/proyecto-cherly'>GITHUB</Link></button>
-            </div>
-          </div>
-        </div>
 
 
       </div>
